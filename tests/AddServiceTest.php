0fbcf367a450715b91548dfb9f026b23a51b3684
Arko-Twid <arkodeep.koley@twidpay.com>
2025-01-28 14:56:21 +0530

    Handled addition for floating numbers, tested for negative integers

--- tests/AddServiceTest.php
+++ tests/AddServiceTest.php
@@ -14,4 +14,29 @@ class AddServiceTest extends TestCase
         $result = $addObject->add($firstNum, $secondNum);
         $this->assertEquals($firstNum+$secondNum, $result);
     }
+    public function testAddTwoNegativeNumber()
+    {
+        $addObject = new AddService();
+        $firstNum=rand(-1,-1000);
+        $secondNum=rand(-1,-1000);
+        $result = $addObject->add($firstNum, $secondNum);
+        $this->assertEquals($firstNum+$secondNum, $result);
+    }
+
+    public function testAddTwoNumber()
+    {
+        $addObject = new AddService();
+        $firstNum=rand(-1000,1000);
+        $secondNum=rand(-1000,1000);
+        $result = $addObject->add($firstNum, $secondNum);
+        $this->assertEquals($firstNum+$secondNum, $result);
+    }
+
+    public function testAddTwoFloatNumber()
+    {
+        $addObject = new AddService();
+        $firstNum = mt_rand() / mt_getrandmax() * 100;
+        $secondNum = mt_rand() / mt_getrandmax() * 100;
+        $this->assertEquals($firstNum+$secondNum, $addObject->add($firstNum, $secondNum));
+    }
 }
